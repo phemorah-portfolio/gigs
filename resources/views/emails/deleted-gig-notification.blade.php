29e37008ca57965e2dbfaadc1156fcf9c9693350
Femi Ajao <phemorah@gmail.com>
2022-09-05 22:59:23 +0100

    Delete Gig notification blade file
    Deleted gig notification blade file to dispatch email notifications template to the admin whenever a gig is deleted

---
+++ resources/views/emails/deleted-gig-notification.blade.php
@@ -0,0 +1,18 @@
+<p>GIG/LISTING DELETED:</p>
+<div>
+    <h3>A Gig/Listing was deleted by {{ $listing->user->name }}</h3>
+</div>
+<style>
+    table, td, th {
+        border: 1px solid #ddd;
+        text-align: left;
+    }
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+    th, td {
+        padding: 15px;
+        text-align: left;
+    }
+</style>
