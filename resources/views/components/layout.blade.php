272bda3b56eb9f6cc76e42ccc1c8238b084ea328
Femi Ajao <phemorah@gmail.com>
2022-09-05 22:56:06 +0100

    Layout default template
    Layout blade file used as default template for the entire pages of this app

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gigs</title>
+</head>
+<body>
+    <h1>Gigs Listing</h1>
+    <!--  VIEW OUTPUT  -->
+    <!-- @yield('content') -->
+    {{$slot}}
+</body>
+</html>
